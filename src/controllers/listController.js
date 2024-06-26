c142c74f588b93eca721c54306fe7082b555e609
Dylan GAVAUD <dylan.gavaud.dev@gmail.com>
2024-06-26 18:41:19 +0200

    Ajout des fonctions createList et getListById

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -2,29 +2,72 @@ import { List } from "../models/index.js";
 
 export async function getAllLists(req, res) {
   try {
-
     // Récupérer toutes les listes en BDD
     const lists = await List.findAll();
-  
+
     // Renvoyer au format JSON
     res.json(lists);
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Unexpected server error. Please try again later." });
+    res
+      .status(500)
+      .json({ error: "Unexpected server error. Please try again later." });
   }
 }
 
 export async function createList(req, res) {
-  // Récupérer les données de la liste à créé fournies par l'utilisateur
-  console.log(req.body);
+  try {
+    // Récupérer les données de la liste à créé fournies par l'utilisateur
+    console.log(req.body);
 
-  // Analyser les données du body pour vérifier que tout est en règle
+    const { title, position } = req.body;
 
-  // Créer la nouvelle liste en BDD
+    if (!title) {
+      return res
+        .status(400)
+        .json({ error: "Missing body parameter: 'title'." });
+    }
 
-  // Répondre au client avec les bonnes infos (cf. la spécification !)
-  res.send("ok"); // TODO: à modifier
+    if (position) {
+      if (isNaN(position)) {
+        return res
+          .status(400)
+          .json({ error: "Invalid type: 'position should be a number." });
+      }
+    }
 
-  // Try-catch
+    // Analyser les données du body pour vérifier que tout est en règle
+
+    // Créer la nouvelle liste en BDD
+    const newList = await List.create({
+      title,
+      position,
+    });
+
+    // Répondre au client avec les bonnes infos (cf. la spécification !)
+    res.json(newList);
+
+    // Try-catch
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Unexpected server error. Please try again later.");
+  }
+}
+
+export async function getListById(req, res) {
+  try {
+    const listId = req.params.id;
+    const list = await List.findByPk(listId);
+
+    if (!list) {
+      return res
+        .status(404)
+        .json({ error: "List not found. Please verify the provided ID." });
+    }
+
+    res.json(list);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Unexpected server error. Please try again later.");
+  }
 }
--- src/router.js
+++ src/router.js
@@ -4,4 +4,7 @@ import * as listController from "./controllers/listController.js";
 export const router = Router();
 
 router.get("/lists", listController.getAllLists);
+router.get("/lists/:id", listController.getListById);
 router.post("/lists", listController.createList);
+// router.patch("/lists/:id")
+// router.delete ("lists/:id")
