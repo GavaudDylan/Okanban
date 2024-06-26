a2843db8e8c1334a5ec0a79631b73a337660b15f
enzoclock <enzo.testa@oclock.io>
2024-06-26 09:37:31 +0200

    feat: modele des Card

--- src/models/dbClientSequelize.js
+++ src/models/dbClientSequelize.js
@@ -11,8 +11,9 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(process.env.PG_URL, {
   define: {
     createdAt: "created_at",
-    updatedAt: "updated_at"
-  }
+    updatedAt: "updated_at",
+    // underscored: true
+  },
 });
 
 // Test rapide
