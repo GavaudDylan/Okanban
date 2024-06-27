6808530045b32f1ea69784f5314ed94b6a27dac2
enzoclock <enzo.testa@oclock.io>
2024-06-27 09:31:23 +0200

    feat: ajout d'une 404

--- src/router.js
+++ src/router.js
@@ -5,3 +5,9 @@ export const router = Router();
 
 router.get("/lists", listController.getAllLists);
 router.post("/lists", listController.createList);
+
+
+// Middleware 404
+router.use((req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
